6cc32f2b25428a51e1c6382a1ad47f23fd81647d
Donald Shtjefni <dnld.sht@gmail.com>
2021-11-16 09:44:05 +0100

    added settings

--- examples/fetch_certificates.js
+++ examples/fetch_certificates.js
@@ -4,42 +4,48 @@ const { X509Certificate, PublicKey } = require('@peculiar/x509');
 const crypto = require('isomorphic-webcrypto');
 const fs = require('fs');
 
-const ENDPOINT = 'https://get.dgc.gov.it/v1/dgc/signercertificate';
-const OUTFILE = 'examples/data/certificates.json';
+const ENDPOINT = 'https://get.dgc.gov.it/v1/dgc';
+const OUTFILE = 'settings.json';
 
 async function main() {
-
-  const certs = await loadKeys()
-  const contents = JSON.stringify(certs, null, '\t') + '\n';
+  const certificates = await loadKeys();
+  const settings = await loadSettings();
+  const contents = `${JSON.stringify({ settings, certificates }, null, '\t')}\n`;
   await fs.promises.writeFile(OUTFILE, contents);
-  const l = Object.values(certs).filter(c => !!c).length
-  console.log(`Wrote ${l} certificates to ${OUTFILE}`);
+
+  console.log(`Wrote settings & certificates to ${OUTFILE}`);
+}
+
+async function loadSettings() {
+  console.log('Loading Settings...');
+  const resp = await fetch(`${ENDPOINT}/settings`);
+  return await resp.json();
 }
 
 async function loadCertificates(resumeToken, keys) {
-  const resp = await fetch(`${ENDPOINT}/update`, {
-    headers: { 'x-resume-token': `${resumeToken}` }
+  const resp = await fetch(`${ENDPOINT}/signercertificate/update`, {
+    headers: { 'x-resume-token': `${resumeToken}` },
   });
   if (resp.status === 204) return keys;
   if (resp.status !== 200) throw new Error(`API returned error: ${await resp.text()}`);
 
-  const headers = resp.headers;
+  const { headers } = resp;
   const newResumeToken = headers.get('x-resume-token');
   const kid = headers.get('x-kid');
-  const certificate = await resp.text()
-  keys[kid] = await parseCert(certificate)
+  const certificate = await resp.text();
+  keys[kid] = await parseCert(certificate);
   return loadCertificates(newResumeToken, keys);
 }
 
 async function loadKeys() {
   console.log('Loading KIDS...');
-  const resp = await fetch(`${ENDPOINT}/status`);
+  const resp = await fetch(`${ENDPOINT}/signercertificate/status`);
   const kids = await resp.json();
-  const keys = kids.reduce((acc, kid) => ({ ...acc, [kid]: null }), {})
+  const keys = kids.reduce((acc, kid) => ({ ...acc, [kid]: null }), {});
 
   console.log(`${kids.length} KIDS found`);
   console.log('Loading certificates...');
-  return await loadCertificates(0, keys)
+  return await loadCertificates(0, keys);
 }
 
 async function parseCert(pem) {
@@ -60,14 +66,14 @@ async function exportCertificate(pem) {
 
   // Export the certificate data.
   return {
-    serialNumber: x509cert.serialNumber,
-    subject: x509cert.subject,
-    issuer: x509cert.issuer,
-    notBefore: x509cert.notBefore.toISOString(),
-    notAfter: x509cert.notAfter.toISOString(),
-    signatureAlgorithm: x509cert.signatureAlgorithm.name,
-    fingerprint: Buffer.from(await x509cert.getThumbprint(crypto)).toString('hex'),
-    ...(await exportPublicKeyInfo(x509cert.publicKey))
+    // serialNumber: x509cert.serialNumber,
+    // subject: x509cert.subject,
+    // issuer: x509cert.issuer,
+    // notBefore: x509cert.notBefore.toISOString(),
+    // notAfter: x509cert.notAfter.toISOString(),
+    // signatureAlgorithm: x509cert.signatureAlgorithm.name,
+    // fingerprint: Buffer.from(await x509cert.getThumbprint(crypto)).toString('hex'),
+    ...(await exportPublicKeyInfo(x509cert.publicKey)),
   };
 }
 
@@ -79,14 +85,14 @@ async function exportCertificate(pem) {
 async function exportPublicAsCert(pem) {
   // Export the certificate data.
   return {
-    serialNumber: '',
-    subject: 'UNKNOWN',
-    issuer: 'UNKNOWN',
-    notBefore: '2020-01-01',
-    notAfter: '2030-01-01',
-    signatureAlgorithm: '',
-    fingerprint: '',
-    ...(await exportPublicKeyInfo(new PublicKey(pem)))
+    // serialNumber: '',
+    // subject: 'UNKNOWN',
+    // issuer: 'UNKNOWN',
+    // notBefore: '2020-01-01',
+    // notAfter: '2030-01-01',
+    // signatureAlgorithm: '',
+    // fingerprint: '',
+    ...(await exportPublicKeyInfo(new PublicKey(pem))),
   };
 }
 
@@ -104,7 +110,7 @@ async function exportPublicKeyInfo(publicKey) {
   // Export the certificate data.
   return {
     publicKeyAlgorithm: public_key.algorithm,
-    publicKeyPem: Buffer.from(spki).toString('base64')
+    publicKeyPem: Buffer.from(spki).toString('base64'),
   };
 }
 
